cc58534d182c683d64b1774549598374c2b6be06
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-10-15 23:22:43 -0400

    feat: add contact service

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,9 +1,62 @@
-import { Injectable } from '@nestjs/common';
-import { Message } from '@contact-list/api-interfaces';
+import { Injectable, NotFoundException  } from '@nestjs/common';
+import { IContact } from '@contact-list/api-interfaces';
+import { Model } from "mongoose";
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { UpdateContactDto } from './dto/update-contact.dto';
 
 @Injectable()
 export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
+
+  constructor(@InjectModel('Contact') private contactModel:Model<IContact>) { }
+
+  async createContact(createContactDto: CreateContactDto): Promise<IContact> {
+    const newContact = await new this.contactModel(createContactDto);
+    return newContact.save();
+  }
+
+  async filterContacts(query): Promise<IContact[]> {
+      const results = await this.contactModel.find(query)
+      return results
+  }
+
+  async updateContact(contactId: string, updateContactDto: UpdateContactDto): Promise<IContact> {
+    const existingContact = await this.contactModel.findByIdAndUpdate(contactId, updateContactDto, { new: true });
+
+    if (!existingContact) {
+      throw new NotFoundException(`Contact #${contactId} not found`);
+    }
+
+    return existingContact;
+  }
+
+  async getAllContacts(): Promise<IContact[]> {
+    const contactData = await this.contactModel.find();
+
+    if (!contactData || contactData.length == 0) {
+      throw new NotFoundException('Contacts not found!');
+    }
+
+    return contactData;
+  }
+
+  async getContact(contactId: string): Promise<IContact> {
+    const existingContact  = await this.contactModel.findById(contactId).exec();
+
+    if (!existingContact ) {
+      throw new NotFoundException(`Contact #${contactId} not found`);
+    }
+
+    return existingContact ;
+  }
+
+  async deleteContact(contactId: string): Promise<IContact> {
+    const deletedContact = await this.contactModel.findByIdAndDelete(contactId);
+
+    if (!deletedContact) {
+      throw new NotFoundException(`Contact #${contactId} not found`);
+    }
+
+    return deletedContact;
   }
 }
