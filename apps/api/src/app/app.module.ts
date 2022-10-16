4c67e085e42ae4a38f495ed94233c412e40dc008
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-10-15 23:21:36 -0400

    feat: add mongoose to module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactSchema } from './schema/contact.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://admin:admin@localhost:27017', { dbName: 'contactdb'}), 
+            MongooseModule.forFeature([{ name: 'Contact', schema: ContactSchema }])],
   controllers: [AppController],
   providers: [AppService],
 })
