ed445fa44a028b87ab8a127b47db92434253e409
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-10-15 23:23:08 -0400

    feat: add contact controller

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
 
   describe('getData', () => {
     it('should return "Welcome to api!"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
+      // const appController = app.get<AppController>(AppController);
+      // expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
     });
   });
 });
--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,15 +1,82 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, Res } from '@nestjs/common';
 
-import { Message } from '@contact-list/api-interfaces';
+// import { IContact } from '@contact-list/api-interfaces';
 
 import { AppService } from './app.service';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { UpdateContactDto } from './dto/update-contact.dto';
 
-@Controller()
+@Controller('contacts')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly contactService: AppService) {}
 
-  @Get('hello')
-  getData(): Message {
-    return this.appService.getData();
+  @Get()
+  async getContacts(@Res() response) {
+    try {
+      const contacts = await this.contactService.getAllContacts();
+      return response.status(HttpStatus.OK).json(contacts);
+    } catch (err) {
+      return response.status(err.status).json(err.response);
+    }
   }
+
+  @Get('filter')
+  async filterContacts(@Query() query, @Res() response) {
+    try {
+      const contacts = await this.contactService.filterContacts(query);
+      return response.status(HttpStatus.OK).json(contacts);
+    } catch (err) {
+      return response.status(err.status).json(err.response);
+    }
+  }
+
+  @Get('/:id')
+  async getContact(@Res() response, @Param('id') contactId: string) {
+    try {
+        const existingContact = await
+        this.contactService.getContact(contactId);
+        return response.status(HttpStatus.OK).json({
+          message: 'Contact found successfully', existingContact
+        });
+    } catch (err) {
+      return response.status(err.status).json(err.response);
+    }
+  }
+
+  @Put(':id')
+  updateContact(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
+    return this.contactService.updateContact(id, updateContactDto);
+  }
+
+  @Post()
+  async createContact(@Res() response, @Body() createContactDto: CreateContactDto) {
+    try {
+      const newContact = await this.contactService.createContact(createContactDto);
+
+      return response.status(HttpStatus.CREATED).json({
+        message: 'Contact has been created successfully',
+        newContact
+      });
+    } catch (err) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: 400,
+        message: 'Error: Contact not created!',
+        error: 'Bad Request'
+      });
+    }
+  }
+
+  @Delete('/:id')
+  async deleteContact(@Res() response, @Param('id') contactId: string) {
+    try {
+      const deletedContact = await this.contactService.deleteContact(contactId);
+      return response.status(HttpStatus.OK).json({
+        message: 'Contact deleted successfully',
+        deletedContact
+      });
+    } catch (err) {
+      return response.status(err.status).json(err.response);
+    }
+  }
+
 }
