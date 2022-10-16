a5eaaed89a261d77ee37664ed199524ca3f7a15e
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-10-15 23:20:28 -0400

    feat: add contact interface

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,8 @@
-export interface Message {
-  message: string;
+import { Document } from 'mongoose';
+
+export interface IContact extends Document {
+    readonly name: string;
+    readonly address: string;
+    readonly phone: string;
+    readonly email: string;
 }
