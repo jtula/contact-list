66e6fe73e70762287345c0b9db34218543a98f0c
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-10-16 19:13:49 -0400

    feat: add contact service

---
+++ apps/contact-list/src/app/services/contact.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { IContact } from '@contact-list/api-interfaces';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private apiUrl = 'http://localhost:3333/api/contacts'
+
+  constructor(private http:HttpClient) { }
+
+  getContacts(): Observable<IContact[]> {
+    return this.http.get<IContact[]>(this.apiUrl)
+  }
+
+  addContact(contact: IContact): Observable<IContact> {
+    return this.http.post<IContact>(this.apiUrl, contact, httpOptions);
+  }
+
+  deleteContact(contact: IContact): Observable<IContact> {
+    const url = `${this.apiUrl}/${contact._id}`
+    return this.http.delete<IContact>(url)
+  }
+}
